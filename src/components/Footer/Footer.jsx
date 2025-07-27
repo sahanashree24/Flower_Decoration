278082659a49c5232d4676296c92aa4b8ed7a27a
sahanashree24 <sahanasundar24@gmail.com>
2025-07-27 18:54:42 +0530

    Initial working version

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,42 +2,42 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer class="footer">
-      <div class="container">
-        <div class="footer__top">
-          <div class="row">
-            <div class="col-lg-6 col-md-6">
-              <div class="footer__top__logo">
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__top">
+          <div className="row">
+            <div className="col-lg-6 col-md-6">
+              <div className="footer__top__logo">
                 <a href="#">
                   <img src="img/logo.png" alt="" />
                 </a>
               </div>
             </div>
-            {/* <div class="col-lg-6 col-md-6">
-              <div class="footer__top__social">
+            {/* <div className="col-lg-6 col-md-6">
+              <div className="footer__top__social">
                 <a href="#">
-                  <i class="fa fa-facebook"></i>
+                  <i className="fa fa-facebook"></i>
                 </a>
                 <a href="#">
-                  <i class="fa fa-twitter"></i>
+                  <i className="fa fa-twitter"></i>
                 </a>
                 <a href="#">
-                  <i class="fa fa-dribbble"></i>
+                  <i className="fa fa-dribbble"></i>
                 </a>
                 <a href="#">
-                  <i class="fa fa-instagram"></i>
+                  <i className="fa fa-instagram"></i>
                 </a>
                 <a href="#">
-                  <i class="fa fa-youtube-play"></i>
+                  <i className="fa fa-youtube-play"></i>
                 </a>
               </div>
             </div> */}
           </div>
         </div>
-        <div class="footer__option">
-          <div class="row">
-            <div class="col-lg-4 col-md-6 col-sm-6">
-              <div class="footer__option__item">
+        <div className="footer__option">
+          <div className="row">
+            <div className="col-lg-4 col-md-6 col-sm-6">
+              <div className="footer__option__item">
                 <h5>About us</h5>
                 <p>
                   Since 2024, we’ve been creating stunning floral decorations
@@ -45,13 +45,13 @@ const Footer = () => {
                   transform spaces into beautiful, memorable experiences. Let us
                   bring elegance to your special moments!
                 </p>
-                <a href="#" class="read__more">
-                  Read more <span class="arrow_right"></span>
+                <a href="#" className="read__more">
+                  Read more <span className="arrow_right"></span>
                 </a>
               </div>
             </div>
-            <div class="col-lg-2 col-md-3 col-sm-3">
-              <div class="footer__option__item">
+            <div className="col-lg-2 col-md-3 col-sm-3">
+              <div className="footer__option__item">
                 <h5>Who we are</h5>
                 <ul>
                   <li>
@@ -69,8 +69,8 @@ const Footer = () => {
                 </ul>
               </div>
             </div>
-            <div class="col-lg-2 col-md-3 col-sm-3">
-              <div class="footer__option__item">
+            <div className="col-lg-2 col-md-3 col-sm-3">
+              <div className="footer__option__item">
                 <h5>Our Work</h5>
                 <ul>
                   <li>
@@ -88,8 +88,8 @@ const Footer = () => {
                 </ul>
               </div>
             </div>
-            <div class="col-lg-4 col-md-12">
-              <div class="footer__option__item">
+            <div className="col-lg-4 col-md-12">
+              <div className="footer__option__item">
                 <h5>Newsletter</h5>
                 <p>
                   Get the latest updates on floral trends, exclusive offers, and
@@ -99,21 +99,21 @@ const Footer = () => {
                 <form action="#">
                   <input type="text" placeholder="Email" />
                   <button type="submit">
-                    <i class="fa fa-send"></i>
+                    <i className="fa fa-send"></i>
                   </button>
                 </form>
               </div>
             </div>
           </div>
         </div>
-        <div class="footer__copyright">
-          <div class="row">
-            <div class="col-lg-12 text-center">
-              <p class="footer__copyright__text">
+        <div className="footer__copyright">
+          <div className="row">
+            <div className="col-lg-12 text-center">
+              <p className="footer__copyright__text">
                 Copyright &copy;
                 <script>document.write(new Date().getFullYear());</script>
                 All rights reserved | Beautiful floral decorations crafted with
-                <i class="fa fa-heart-o" aria-hidden="true"></i> by
+                <i className="fa fa-heart-o" aria-hidden="true"></i> by
                 <a href="#" target="_blank">
                   DK Decorators
                 </a>
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import tailwindcss from '@tailwindcss/vite';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const isProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Flower_Decoration/' : '/',
+  base: isProd ? '/Flower_Decoration/' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
-    alias: {
-      '@css': path.resolve(__dirname, 'src/styles/css'),
-    },
+    alias: { '@css': path.resolve(__dirname, 'src/styles/css') },
   },
 });
+
+
+
+
 
 
